0b4edd3b492aa05325cc9c8353099d89822a2491
flashbLack98 <npalakuttige98@icloud.com>
2021-10-07 15:37:25 +0200

    fine esercizio

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+const nomeUtente = prompt("inserici nome")
+const cognomeUtente = prompt("inserisci cognome")
+const coloreUtente = prompt("inserici colore preferito")
+
+console.log (nomeUtente)
+console.log (cognomeUtente)
+console.log (coloreUtente)
+
+alert(nomeUtente + cognomeUtente + coloreUtente + "21")
